d0d289c03fc982c93b6576b870ebb7ac911fda9e
Dagtret <sergeyaggo2015@gmail.com>
2025-05-03 13:47:27 +0300

    Сделал тест получение списка питомцев по статусу

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,6 @@
 import allure
 import jsonschema
+import pytest
 import requests
 from .schemas.pet_schema import PET_SCHEMA
 
@@ -135,3 +136,21 @@ class TestPet:
             assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
         with allure.step("Проверка текстового содержания ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+    @allure.title("Получение списка питомцев по статусу")
+    @pytest.mark.parametrize(
+        "status, expected_status_code",
+        [
+            ("available", 200),
+            ("pending", 200),
+            ("sold", 200),
+            ("-", 400)
+        ]
+
+    )
+    def test_get_pet_by_status(self, status, expected_status_code):
+        with allure.step(f"Отправка запроса на получение питомцев по статусу {status}"):
+            response = requests.get(url=f"{BASE_URL}/pet/findByStatus", params={"status": status})
+        with allure.step("Проверка статуса ответа и формата данных"):
+            assert response.status_code == expected_status_code
+            assert isinstance(response.json(), list)
